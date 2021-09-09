b86079497a9931df7c5e6e47e03b800e19a35aba
ekta-kakadia <ekat#improwised.com>
2021-09-09 14:57:48 +0530

    Add function of user added

--- app/src/components/Users.vue
+++ app/src/components/Users.vue
@@ -5,8 +5,91 @@
         <v-col cols="8">Users </v-col>
       </v-row>
       <v-spacer></v-spacer>
+      <div class="pl-5 pr-5 text-right">
+        <v-row>
+          <v-col cols="3">
+            <v-dialog v-model="dialog" width="500">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                  Add User
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title class="text-h5 grey lighten-2">
+                  Add User
+                </v-card-title>
+
+                <v-card-text>
+                  <div class="my-4 text-subtitle-1">
+                    <v-form ref="form" v-model="valid" lazy-validation>
+                      <v-text-field
+                        v-model="form.first_name"
+                        label="First Name"
+                        required
+                        :rules="nameRules"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="form.last_name"
+                        label="Last Name"
+                        required
+                        :rules="nameRules"
+                      ></v-text-field>
+                      <v-text-field
+                        type="email"
+                        v-model="form.email"
+                        label="Email"
+                        required
+                        :rules="nameRules"
+                      ></v-text-field>
+                      <v-text-field
+                        type="password"
+                        v-model="form.password"
+                        label="Password"
+                        required
+                        :rules="nameRules"
+                      ></v-text-field>
+                      <v-text-field
+                        :rules="nameRules"
+                        type="number"
+                        v-model="form.number"
+                        label="Phone Number"
+                        length="10"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        :rules="nameRules"
+                        v-model="form.company"
+                        label="Company"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        :rules="nameRules"
+                        v-model="form.username"
+                        label="Username"
+                        required
+                      ></v-text-field>
+                      <v-divider></v-divider>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn text @click="dialog = false"> Close </v-btn>
+                        <v-btn
+                          color="primary"
+                          :disabled="!valid"
+                          @click="addUser"
+                        >
+                          Add
+                        </v-btn>
+                      </v-card-actions>
+                    </v-form>
+                  </div>
+                </v-card-text>
+              </v-card>
+            </v-dialog>
+          </v-col>
+        </v-row>
+      </div>
     </v-card-title>
-   
 
     <div>
       <v-simple-table>
@@ -28,11 +111,15 @@
                   item.id
                 }}</router-link>
               </td>
-              <td>{{ item.first_name }} {{item.last_name}}</td>
-              <td>{{item.company}}</td>
+              <td>{{ item.first_name }} {{ item.last_name }}</td>
+              <td>{{ item.company }}</td>
               <td>{{ item.email }}</td>
-              <td>{{item.username}}</td>
-              <td><router-link :to="{path: `/user/edit/${item.id}`}">Edit</router-link></td>
+              <td>{{ item.username }}</td>
+              <td>
+                <router-link :to="{ path: `/user/edit/${item.id}` }"
+                  >Edit</router-link
+                >
+              </td>
             </tr>
           </tbody>
         </template>
@@ -42,6 +129,22 @@
 </template>
 <script>
 export default {
+  data() {
+    return {
+      nameRules: [(v) => !!v || "required"],
+      valid: true,
+      dialog: false,
+      form: {
+        first_name: "",
+        last_name: "",
+        email: "",
+        password: "",
+        username: "",
+        company: "",
+        number: "",
+      },
+    };
+  },
   mounted() {
     this.getUsers();
   },
@@ -50,9 +153,30 @@ export default {
       this.loading = true;
       await this.$store
         .dispatch("users")
-        .then(() => {
-        })
+        .then(() => {})
         .catch((e) => e);
+    },
+    addUser() {
+      if (this.$refs.form.validate()) {
+        this.$store
+          .dispatch("addUser", this.form)
+          .then(() => {
+            this.dialog = false;
+            this.$toasted.success("User added successfully", {
+              theme: "bubble",
+              position: "top-right",
+              duration: 4000,
+            });
+            this.$store.dispatch("users");
+          })
+          .catch((e) => e);
+      } else {
+        this.$toasted.success("Invalid details entered", {
+          theme: "bubble",
+          position: "top-right",
+          duration: 4000,
+        });
+      }
     },
   },
   computed: {
